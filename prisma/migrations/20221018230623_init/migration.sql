4c8efe8e72539d00116b3bee5b85018b9b2f79ba
jpmmatias <jpmmatias2018@outlook.com>
2022-10-18 21:45:01 -0300

    Connection do database, created schema and seed

---
+++ prisma/migrations/20221018230623_init/migration.sql
@@ -0,0 +1,79 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "name" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "Artist" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Artist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Song" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "artistId" INTEGER NOT NULL,
+
+    CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Playlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PlaylistToSong" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Artist_name_key" ON "Artist"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PlaylistToSong_AB_unique" ON "_PlaylistToSong"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PlaylistToSong_B_index" ON "_PlaylistToSong"("B");
+
+-- AddForeignKey
+ALTER TABLE "Song" ADD CONSTRAINT "Song_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "Artist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Playlist" ADD CONSTRAINT "Playlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_A_fkey" FOREIGN KEY ("A") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_B_fkey" FOREIGN KEY ("B") REFERENCES "Song"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,67 @@
+import { PrismaClient } from '@prisma/client';
+import bycript from 'bcrypt';
+import { artistsData } from './songsData';
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+	await Promise.all(
+		artistsData.map(async (artist) => {
+			return prisma.artist.upsert({
+				where: { name: artist.name },
+				update: {},
+				create: {
+					name: artist.name,
+					songs: {
+						create: artist.songs.map((song) => ({
+							name: song.name,
+							duration: song.duration,
+							url: song.url,
+						})),
+					},
+				},
+			});
+		})
+	);
+
+	const salt = bycript.genSaltSync();
+
+	const user = await prisma.user.upsert({
+		where: { email: 'user@email.com' },
+		update: {},
+		create: {
+			email: 'user@email.com',
+			password: bycript.hashSync('senha', salt),
+			name: 'User',
+		},
+	});
+
+	const songs = await prisma.song.findMany({});
+	await Promise.all(
+		new Array(10).fill(1).map((_, i) => {
+			return prisma.playlist.create({
+				data: {
+					name: `Playlist ${i + 1}`,
+					user: {
+						connect: { id: user.id },
+					},
+					songs: {
+						connect: songs.map((song) => ({
+							id: song.id,
+						})),
+					},
+				},
+			});
+		})
+	);
+};
+
+run()
+	.then()
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
